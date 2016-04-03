a972142f9728abc2ddc6f2faf3ebea1ef3e91385
Saikrishna Arcot <saiarcot895@gmail.com>
2016-04-03 11:35:22 -0400

    More web page functionality.

--- Sample Area Chart/main.js
+++ Sample Area Chart/main.js
@@ -12,16 +12,42 @@ function start()
                                 .y(function (d) { return d[1]; })
                                 .useInteractiveGuideline(false)
                                 .showControls(false)
-								.showLegend(false)
+                                .showLegend(false)
                                 .clipEdge(true);
 
             chart.xAxis.tickFormat(function (d) { return d3.time.format('%x')(new Date(d)) });
 
             chart.yAxis.tickFormat(d3.format(',.d'));
 
+            for (var i = 0; i < data.length; i++) {
+                data[i].disabled = (i >= 10);
+            }
+
             d3.select("#chart").datum(data).call(chart);
 
+            d3.select("#top10Button").on('click', function() {
+                for (var i = 0; i < data.length; i++) {
+                    data[i].disabled = (i >= 10);
+                }
+                chart.update();
+            });
+
+            d3.select("#top25Button").on('click', function() {
+                for (var i = 0; i < data.length; i++) {
+                    data[i].disabled = (i >= 25);
+                }
+                chart.update();
+            });
+
+            d3.select("#allButton").on('click', function() {
+                for (var i = 0; i < data.length; i++) {
+                    data[i].disabled = false;
+                }
+                chart.update();
+            });
+
             nv.utils.windowResize(chart.update);
+
             return chart;
         });
     });
