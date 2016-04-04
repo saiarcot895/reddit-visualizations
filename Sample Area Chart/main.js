f8c61e5865b8e600eff5c8935f2152a3e6d2f2f0
Saikrishna Arcot <saiarcot895@gmail.com>
2016-04-03 20:10:54 -0400

    Website updates.

--- Sample Area Chart/main.js
+++ Sample Area Chart/main.js
@@ -19,32 +19,55 @@ function start()
 
             chart.yAxis.tickFormat(d3.format(',.d'));
 
-            for (var i = 0; i < data.length; i++) {
-                data[i].disabled = (i >= 10);
-            }
+            data.forEach(function(d, i) {
+                d.disabled = (i >= 10);
+            });
 
             d3.select("#chart").datum(data).call(chart);
 
-            d3.select("#top10Button").on('click', function() {
-                for (var i = 0; i < data.length; i++) {
-                    data[i].disabled = (i >= 10);
-                }
+            /*d3.select("#top10Button").on('click', function() {
+                data.forEach(function(d, i) {
+                    d.disabled = (i >= 10);
+                });
                 chart.update();
             });
 
             d3.select("#top25Button").on('click', function() {
-                for (var i = 0; i < data.length; i++) {
-                    data[i].disabled = (i >= 25);
-                }
+                data.forEach(function(d, i) {
+                    d.disabled = (i >= 25);
+                });
                 chart.update();
             });
 
             d3.select("#allButton").on('click', function() {
-                for (var i = 0; i < data.length; i++) {
-                    data[i].disabled = false;
-                }
+                data.forEach(function(d, i) {
+                    d.disabled = false;
+                });
                 chart.update();
-            });
+            });*/
+
+            d3.select(".panel-body")
+                .selectAll("button")
+                .data(data)
+                .enter()
+                .append("button")
+                .classed("btn btn-primary", true)
+                .classed("active", function(d) {
+                    return !d.disabled;
+                })
+                .attr("type", "button")
+                .attr("data-toggle", "button")
+                .attr("autocomplete", "off")
+                .attr("aria-pressed", function(d) {
+                    return !d.disabled;
+                })
+                .text(function(d) {
+                    return d.key;
+                })
+                .on("click", function(d) {
+                    d.disabled = !d.disabled;
+                    chart.update();
+                });
 
             nv.utils.windowResize(chart.update);
 
